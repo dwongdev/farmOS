5310a261245d1e157f7e0d64a81d2688874b2440
Paul Weidner <paul.weidner@gmail.com>
2025-02-13 16:46:39 -0800

    Ignore PHPstan error for unnecessary container service check
    Fixes If condition is always true.

--- modules/core/kml/src/FarmKmlServiceProvider.php
+++ modules/core/kml/src/FarmKmlServiceProvider.php
@@ -16,6 +16,9 @@ class FarmKmlServiceProvider implements ServiceModifierInterface {
    * {@inheritdoc}
    */
   public function alter(ContainerBuilder $container) {
+    // Ignore PHPStan error for conditional logic.
+    // We check that the container service exists for extra safety.
+    // @phpstan-ignore booleanAnd.leftAlwaysTrue
     if ($container->has('http_middleware.negotiation') && is_a($container->getDefinition('http_middleware.negotiation')->getClass(), '\Drupal\Core\StackMiddleware\NegotiationMiddleware', TRUE)) {
       $container->getDefinition('http_middleware.negotiation')->addMethodCall('registerFormat', ['kml', ['application/vnd.google-earth.kml+xml']]);
     }
