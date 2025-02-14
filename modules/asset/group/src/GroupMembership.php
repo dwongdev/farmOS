6a15aa5cad34f357d86e2f7e3247804cc8d1f4b3
Paul Weidner <paul.weidner@gmail.com>
2025-02-13 16:01:00 -0800

    Return result of entity storage load()
    Fixes Call to function is_null() with Drupal\farm_map\Entity\LayerStyleInterface will always evaluate to false
    
    Unreachable statement - code above always terminates

--- modules/asset/group/src/GroupMembership.php
+++ modules/asset/group/src/GroupMembership.php
@@ -133,17 +133,8 @@ class GroupMembership implements GroupMembershipInterface {
       return NULL;
     }
 
-    // Load the first log.
-    /** @var \Drupal\log\Entity\LogInterface $log */
-    $log = $this->entityTypeManager->getStorage('log')->load(reset($log_ids));
-
-    // Return the log, if available.
-    if (!is_null($log)) {
-      return $log;
-    }
-
-    // Otherwise, return NULL.
-    return NULL;
+    // Load the first log or return NULL.
+    return $this->entityTypeManager->getStorage('log')->load(reset($log_ids));
   }
 
   /**
