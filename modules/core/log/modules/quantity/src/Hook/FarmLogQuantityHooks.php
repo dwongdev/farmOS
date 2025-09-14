fc987459ac592bf5ce1daeba2ab4400d0d19d94b
Michael Stenta <mike@mstenta.net>
2025-09-14 15:33:50 -0400

    Move #[Hook('form_log_form_alter')] from FarmLogHooks to FarmLogQuantityHooks.
    This method calls farm_log_quantity_default_type(), which is
    only available if the farm_log_quantity module is enabled.

--- modules/core/log/modules/quantity/src/Hook/FarmLogQuantityHooks.php
+++ modules/core/log/modules/quantity/src/Hook/FarmLogQuantityHooks.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Drupal\farm_log_quantity\Hook;
 
 use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\Entity\EntityFormInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\farm_field\FarmFieldFactoryInterface;
 
@@ -51,6 +53,31 @@ class FarmLogQuantityHooks {
     }
 
     return $fields;
+  }
+
+  /**
+   * Implements hook_form_BASE_FORM_ID_alter().
+   */
+  #[Hook('form_log_form_alter')]
+  public function formLogFormAlter(&$form, FormStateInterface $form_state, $form_id) {
+
+    // Alter the Quantity inline entity form to set the default quantity type.
+    if (!empty($form['quantity']['widget']['actions']['bundle']['#options'])) {
+      $bundle_select = &$form['quantity']['widget']['actions']['bundle'];
+
+      // Load the log type storage.
+      assert($form_state->getFormObject() instanceof EntityFormInterface);
+      /** @var \Drupal\log\Entity\Log $entity */
+      $entity = $form_state->getFormObject()->getEntity();
+
+      // Determine the default quantity type.
+      $default_type = farm_log_quantity_default_type($entity->bundle());
+
+      // Set the default value.
+      if (array_key_exists($default_type, $bundle_select['#options'])) {
+        $bundle_select['#default_value'] = $default_type;
+      }
+    }
   }
 
 }
--- modules/core/log/src/Hook/FarmLogHooks.php
+++ modules/core/log/src/Hook/FarmLogHooks.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\farm_log\Hook;
 
 use Drupal\Core\DependencyInjection\AutowireTrait;
-use Drupal\Core\Entity\EntityFormInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
@@ -64,31 +63,6 @@ class FarmLogHooks {
         }
       }
       $entity->set('asset', $asset_ids);
-    }
-  }
-
-  /**
-   * Implements hook_form_BASE_FORM_ID_alter().
-   */
-  #[Hook('form_log_form_alter')]
-  public function formLogFormAlter(&$form, FormStateInterface $form_state, $form_id) {
-
-    // Alter the Quantity inline entity form to set the default quantity type.
-    if (!empty($form['quantity']['widget']['actions']['bundle']['#options'])) {
-      $bundle_select = &$form['quantity']['widget']['actions']['bundle'];
-
-      // Load the log type storage.
-      assert($form_state->getFormObject() instanceof EntityFormInterface);
-      /** @var \Drupal\log\Entity\Log $entity */
-      $entity = $form_state->getFormObject()->getEntity();
-
-      // Determine the default quantity type.
-      $default_type = farm_log_quantity_default_type($entity->bundle());
-
-      // Set the default value.
-      if (array_key_exists($default_type, $bundle_select['#options'])) {
-        $bundle_select['#default_value'] = $default_type;
-      }
     }
   }
 
