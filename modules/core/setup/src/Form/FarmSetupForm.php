945155d2e0b338f3296d3c7aee3b3f5991a1cc0b
Michael Stenta <mike@mstenta.net>
2025-11-26 07:51:42 -0500

    Add a wrapper form for building/validating/submitting setup forms.

---
+++ modules/core/setup/src/Form/FarmSetupForm.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\farm_setup\Form;
+
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\farm_setup\SetupFormPluginManager;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
+/**
+ * Setup form.
+ *
+ * @ingroup farm
+ */
+class FarmSetupForm extends FormBase {
+
+  use AutowireTrait;
+
+  public function __construct(
+    #[Autowire(service: 'plugin.manager.setup_form')]
+    protected SetupFormPluginManager $setupFormPluginManager,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'farm_setup_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, ?string $plugin_id = NULL) {
+
+    // Build the setup form.
+    $form = $this->setupFormPluginManager->createInstance($plugin_id)->buildForm($form, $form_state);
+
+    // Save the plugin ID for validate/submit.
+    $form['plugin_id'] = [
+      '#type' => 'value',
+      '#value' => $plugin_id,
+    ];
+
+    // Submit buttons.
+    // If this is the welcome plugin, only show "Continue".
+    // Or, if this is the resources plugin, only show "Finish".
+    // Otherwise, show "Save and continue" (which submits the form), and "Skip"
+    // (which goes to the next form without submitting).
+    $form['setup']['actions'] = ['#type' => 'actions'];
+    if ($plugin_id == 'welcome') {
+      $form['setup']['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Continue'),
+        '#submit' => [[$this, 'skipSubmit']],
+      ];
+    }
+    elseif ($plugin_id == 'resources') {
+      $form['setup']['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Finish'),
+        '#submit' => [[$this, 'skipSubmit']],
+      ];
+    }
+    else {
+      $form['setup']['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Save and continue'),
+      ];
+      $form['setup']['actions']['skip'] = [
+        '#type' => 'submit',
+        '#value' => $this->t('Skip'),
+        // Ignore all validation errors, but preserve the plugin_id value.
+        '#limit_validation_errors' => [['plugin_id']],
+        '#submit' => [[$this, 'skipSubmit']],
+      ];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $plugin_id = $form_state->getValue('plugin_id');
+    $this->setupFormPluginManager->createInstance($plugin_id)->validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->setupFormPluginManager->createInstance($form_state->getValue('plugin_id'))->submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function skipSubmit(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
