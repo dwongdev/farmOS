3c958187f4e7ab0395449af4079d9a90e57f9c37
Michael Stenta <mike@mstenta.net>
2026-02-20 13:24:50 -0500

    Improve farm_ui_action tests by checking action link URLs in addition to text.

--- modules/core/ui/action/tests/src/Functional/ActionsTest.php
+++ modules/core/ui/action/tests/src/Functional/ActionsTest.php
@@ -68,43 +68,43 @@ class ActionsTest extends FarmBrowserTestBase {
     // Test dashboard buttons.
     $this->drupalGet('/dashboard');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Asset');
-    $this->assertSession()->linkExists('Add Log');
-    $this->assertSession()->linkExists('Add Organization');
-    $this->assertSession()->linkExists('Add Plan');
+    $this->assertActionLinkExists('Add Asset', '/asset/add');
+    $this->assertActionLinkExists('Add Log', '/log/add');
+    $this->assertActionLinkExists('Add Organization', '/organization/add');
+    $this->assertActionLinkExists('Add Plan', '/plan/add');
 
     // Test entity lists.
     $this->drupalGet('/assets');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Asset');
+    $this->assertActionLinkExists('Add Asset', '/asset/add');
     $this->drupalGet('/logs');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log');
+    $this->assertActionLinkExists('Add Log', '/log/add');
     $this->drupalGet('/organizations');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Organization');
+    $this->assertActionLinkExists('Add Organization', '/organization/add');
     $this->drupalGet('/plans');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Plan');
+    $this->assertActionLinkExists('Add Plan', '/plan/add');
 
     // Test per-bundle entity lists.
     $this->drupalGet('/assets/test');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Asset: Test');
+    $this->assertActionLinkExists('Add Asset: Test', '/asset/add/test');
     $this->drupalGet('/logs/test');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log: Test');
+    $this->assertActionLinkExists('Add Log: Test', '/log/add/test');
     $this->drupalGet('/organizations/test');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Organization: Test');
+    $this->assertActionLinkExists('Add Organization: Test', '/organization/add/test');
     $this->drupalGet('/plans/test');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Plan: Test');
+    $this->assertActionLinkExists('Add Plan: Test', '/plan/add/test');
 
     // Test /user/%uid/logs.
     $this->drupalGet('/user/' . $this->user->id() . '/logs');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log');
+    $this->assertActionLinkExists('Add Log', '/log/add');
 
     // Test links to /log/add/[bundle]?asset=[id] on asset pages.
     /** @var \Drupal\asset\Entity\AssetInterface $asset */
@@ -121,13 +121,26 @@ class ActionsTest extends FarmBrowserTestBase {
     $log->save();
     $this->drupalGet('/asset/' . $asset->id());
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log: Test');
+    $this->assertActionLinkExists('Add Log: Test', '/log/add/test?asset=' . $asset->id());
     $this->drupalGet('/asset/' . $asset->id() . '/logs');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log: Test');
+    $this->assertActionLinkExists('Add Log: Test', '/log/add/test?asset=' . $asset->id());
     $this->drupalGet('/asset/' . $asset->id() . '/logs/test');
     $this->assertSession()->statusCodeEquals(200);
-    $this->assertSession()->linkExists('Add Log: Test');
+    $this->assertActionLinkExists('Add Log: Test', '/log/add/test?asset=' . $asset->id());
+  }
+
+  /**
+   * Helper method to test that an action link exists.
+   *
+   * @param string $label
+   *   The action link label.
+   * @param string $href
+   *   The action link href.
+   */
+  protected function assertActionLinkExists(string $label, string $href): void {
+    $this->assertSession()->linkExists($label);
+    $this->assertSession()->linkByHrefExists($href);
   }
 
 }
