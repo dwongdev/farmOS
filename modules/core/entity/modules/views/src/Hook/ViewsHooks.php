eee4685971b5ea9e088a75210bd3de3bdc187fc7
Michael Stenta <mike@mstenta.net>
2026-03-08 12:21:35 -0400

    Move entity_reference filter plugin alter logic from farm_ui_views to farm_entity_views.
    This is a lower-level change that could be useful without
    farm_ui_views, and is similar to other alterations already
    being made by farm_entity_views.

--- modules/core/entity/modules/views/src/Hook/ViewsHooks.php
+++ modules/core/entity/modules/views/src/Hook/ViewsHooks.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\farm_entity_views\Hook;
 
+use Drupal\Core\DependencyInjection\AutowireTrait;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 
 /**
@@ -11,17 +13,35 @@ use Drupal\Core\Hook\Attribute\Hook;
  */
 class ViewsHooks {
 
+  use AutowireTrait;
+
+  public function __construct(
+    protected EntityFieldManagerInterface $entityFieldManager,
+  ) {}
+
   /**
    * Implements hook_views_data_alter().
    */
   #[Hook('views_data_alter')]
   public function viewsDataAlter(array &$data) {
 
+    // Use core entity_reference filter plugin for all entity reference fields.
+    // @todo Refactor/remove this when the following core issues are resolved.
+    // @see https://www.drupal.org/project/drupal/issues/3458099
+    // @see https://www.drupal.org/project/drupal/issues/3438054
+    $entity_reference_field_map = $this->entityFieldManager->getFieldMapByFieldType('entity_reference');
+    foreach ($entity_reference_field_map as $entity_type_id => $fields) {
+      foreach ($fields as $field_name => $map) {
+        if (!empty($data[$entity_type_id . '__' . $field_name][$field_name . '_target_id']['filter'])) {
+          $data[$entity_type_id . '__' . $field_name][$field_name . '_target_id']['filter']['id'] = 'entity_reference';
+        }
+      }
+    }
+
+    // Add support for state_machine filters.
     // Because Drupal core does not provide full Views integration for base
     // fields we must manually add support for certain fields.
     // Workaround for core issue #2489476.
-
-    // Add support for state_machine filters.
     $status_filter = [
       'id' => 'state_machine_state',
       'field_name' => 'status',
