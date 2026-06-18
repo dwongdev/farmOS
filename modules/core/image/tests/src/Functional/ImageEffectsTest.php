04fb7acdf3ac902de8fe2318bc29fedb266b03f6
Michael Stenta <mike@mstenta.net>
2026-06-18 15:24:37 -0400

    Improve separation of farm_image automated tests.

--- modules/core/image/tests/src/Functional/ImageEffectsTest.php
+++ modules/core/image/tests/src/Functional/ImageEffectsTest.php
@@ -25,16 +25,50 @@ class ImageEffectsTest extends FarmBrowserTestBase {
   ];
 
   /**
+   * Run all tests.
+   */
+  public function testAll() {
+    $this->doTestEffectAdded();
+    $this->doTestAutoOrient();
+  }
+
+  /**
+   * Tests that the auto-orient effect is added to core styles.
+   */
+  public function doTestEffectAdded(): void {
+
+    // Load the core image styles and confirm that they contain the auto-orient
+    // effect.
+    $styles = [
+      'large',
+      'medium',
+      'thumbnail',
+      'wide',
+    ];
+    foreach ($styles as $style) {
+      $image_style = ImageStyle::load($style);
+      $this->assertNotEmpty(array_filter([...$image_style->getEffects()], function ($effect) {
+        return $effect->getPluginId() === 'farm_image_auto_orient';
+      }));
+    }
+  }
+
+  /**
    * Tests the auto-orient effect.
    */
-  public function testAutoOrient(): void {
+  public function doTestAutoOrient(): void {
 
-    // Load the large image style and confirm that it contains the auto-orient
-    // effect.
-    $image_style = ImageStyle::load('large');
-    $this->assertNotEmpty(array_filter([...$image_style->getEffects()], function ($effect) {
-      return $effect->getPluginId() === 'farm_image_auto_orient';
-    }));
+    // Create a test image style with the auto-orient effect.
+    $image_style = ImageStyle::create([
+      'name' => 'test',
+      'label' => 'Test',
+    ]);
+    $this->assertEquals(SAVED_NEW, $image_style->save());
+    $image_style->addImageEffect([
+      'id' => 'farm_image_auto_orient',
+      'weight' => 0,
+    ]);
+    $this->assertEquals(SAVED_UPDATED, $image_style->save());
 
     // Create a copy of a test image file in public files directory.
     $test_uri = 'public://rotate90cw.jpg';
@@ -48,7 +82,7 @@ class ImageEffectsTest extends FarmBrowserTestBase {
     $this->assertEquals('400', $image_size[0]);
 
     // Execute the image style on the test image via a GET request.
-    $derivative_uri = 'public://styles/large/public/rotate90cw.jpg.webp';
+    $derivative_uri = 'public://styles/test/public/rotate90cw.jpg';
     $this->assertFileDoesNotExist($derivative_uri);
     $url = \Drupal::service('file_url_generator')->transformRelative($image_style->buildUrl($test_uri));
     $this->drupalGet($this->getAbsoluteUrl($url));
@@ -57,8 +91,8 @@ class ImageEffectsTest extends FarmBrowserTestBase {
 
     // Confirm that the derivative file is horizontally oriented.
     $image_size = getimagesize($derivative_uri);
-    $this->assertEquals('320', $image_size[1]);
-    $this->assertEquals('480', $image_size[0]);
+    $this->assertEquals('400', $image_size[1]);
+    $this->assertEquals('600', $image_size[0]);
   }
 
 }
